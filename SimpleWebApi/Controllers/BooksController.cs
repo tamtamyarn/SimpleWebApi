83575c713f6564d1cbe0f7d2c3cc50472db12d6c
tamtamyarn <tamtamyarn@outlook.com>
2019-04-04 23:17:31 +0900

    Initial commit.

---
+++ SimpleWebApi/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SimpleWebApi.Data;
+
+namespace SimpleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookContext context;
+
+        public BooksController(BookContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var books = context.Books;
+            return Ok(books);
+        }
+    }
+}
